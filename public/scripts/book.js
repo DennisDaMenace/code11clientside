9dd3849ca53c492fdb4d1e02a43b1769bb8da746
AaronBruce <aaron.bruce@comcast.net>
2018-01-06 15:53:05 -0800

    fixed typo in book.js, changed author to authors

--- public/scripts/book.js
+++ public/scripts/book.js
@@ -1,6 +1,6 @@
 'use strict';
 
-$('#submitNewButton', '#submitUpdateButton').on('click', () => $.post('/PLACEHOLDER-URL(NEED TO CHANGE)', { author: this.author, title: this.title }))
+$('#submitNewButton', '#submitUpdateButton').on('click', () => $.post('/PLACEHOLDER-URL(NEED TO CHANGE)', { authors: this.authors, title: this.title }))
   .then(data => {
     console.log(data);
     if (callback) callback();
