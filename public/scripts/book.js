5eea99bb7f9cb8f099dac5269b0510532096f2a5
AaronBruce <aaron.bruce@comcast.net>
2018-01-07 15:47:43 -0800

    added functionality for posting/updating/deleting/

--- public/scripts/book.js
+++ public/scripts/book.js
@@ -1,7 +1,7 @@
 'use strict';
 
-$('#submitNewButton', '#submitUpdateButton').on('click', () => $.post('/PLACEHOLDER-URL(NEED TO CHANGE)', { authors: this.authors, title: this.title }))
-  .then(data => {
-    console.log(data);
-    if (callback) callback();
-  });
+// $('#submitNewButton', '#submitUpdateButton').on('click', () => $.post('/PLACEHOLDER-URL(NEED TO CHANGE)', { authors: this.authors, title: this.title }))
+//   .then(data => {
+//     console.log(data);
+//     if (callback) callback();
+//   });
